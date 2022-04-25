02cce58d01818416b5f6e4922946e8a7ae27ccd1
Justin Murphy <98184998+Justin-Murphy-2003@users.noreply.github.com>
2022-04-25 13:59:03 -0400

    Update library.py

---
+++ library.py
@@ -0,0 +1,20 @@
+import requests
+import ctypes
+
+def set_image_as_background(img_path):
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, img_path, 0)
+
+def download_image_from_url(img_url, save_path):
+
+    print('Downloading image from URL...')
+    response = requests.get(img_url)
+
+    if response.status_code == 200:
+        print("success!")
+        img_data = response.content
+        with open(save_path, 'wb') as file:
+                file.write(img_data)
+    else:
+        print("failed. Response code", response.status_code)
+
+    
