f8fdeabf8ec9f5de4f7af6ec076b9196966e73d4
Justin Murphy <98184998+Justin-Murphy-2003@users.noreply.github.com>
2022-04-25 13:59:11 -0400

    Update pokeapi.py

--- pokeapi.py
+++ pokeapi.py
@@ -5,16 +5,19 @@ def get_pokemon_info(name):
     Gets dictionary of info from PokeAPI for specified pokemon.
 
     :param name: Specified Pokemon's Name or index number
+    :returns: Dictionary of pokemon info if success, None if unsuccessful
     """
 
     print("Getting Pokemon information...", end="")
 
     if name is None:
+        print('error: Missing name parameter')
         return
 
     name = name.lower().strip()
 
     if name == '':
+        print('error: Empty name parameter')
         return
 
     URL = 'https://pokeapi.co/api/v2/pokemon/'+str(name)
@@ -26,3 +29,30 @@ def get_pokemon_info(name):
     else:
         print("failed. Response code",response.status_code)
         return
+
+def get_pokemon_list(limit=2000, offset=0):
+    """
+    Gets list of pokemon from PokeAPI
+    """
+    print('Getting list of Pokemon...')
+
+    URL = 'https://pokeapi.co/api/v2/pokemon/'
+    parameters = {
+        'offset' : offset,
+        'limit' : limit
+    }
+    
+    response = requests.get(URL,  params=parameters)
+
+    if response.status_code == 200:
+        print("success!")
+        poke_dict = response.json()
+        return [p['name'] for p in poke_dict['results']] #return names of allpokemon in list
+    else:
+        print("failed. Response code",response.status_code)
+        return
+
+def get_pokemon_image_url(name):
+    poke_dict = get_pokemon_info(name)
+    poke_url = poke_dict['sprites']['other']['official-artwork']['front_default']
+    return poke_url
